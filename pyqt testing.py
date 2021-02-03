5bc20a2e4f8e88952ae2bb4e3cd5abc77598d361
RafaelAchmedov <rafael.achmedov@gmail.com>
2021-02-03 15:58:59 +0200

    5 test commit

--- pyqt testing.py
+++ pyqt testing.py
@@ -2,20 +2,22 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+#komentaras komentaras
+
 class MyWindow(QMainWindow):
    def __init__(self):
       super(MyWindow, self).__init__()
       self.setGeometry(1500, 350, 500, 600)
-      self.setWindowTitle("Techasas with Rafa")
+      self.setWindowTitle("blabla with Rafa")
       self.initUI()
 
    def initUI(self):
       self.label = QtWidgets.QLabel(self)
-      self.label.setText("Rafa first Label")
+      self.label.setText("Rafa first blabla")
       self.label.move(50, 50)
 
       self.b1 = QtWidgets.QPushButton(self)
-      self.b1.setText("Click me Please")
+      self.b1.setText("Click me blabla")
       self.b1.clicked.connect(self.clicked)
 
 
@@ -23,12 +25,12 @@ class MyWindow(QMainWindow):
       self.label.adjustSize()
 
    def clicked(self):
-      self.label.setText("You pressed the button")
+      self.label.setText("blabla pressed the button")
       self.update()
 
 
 def clicked():
-   print("Clicked yesy esy esy esyeyese")
+   print("Clicked blabla")
 
 
 def window():
